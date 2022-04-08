cd8b1eb54848d0fd1b1828746ff09d04d521397d
eratiani <62502052+eratiani@users.noreply.github.com>
2022-04-08 19:05:36 +0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,46 @@
+const el = document.getElementById('box');
+
+const hiddenEl = document.getElementById('hidden');
+
+el.addEventListener('mouseover', function handleMouseOver() {
+    hiddenEl.style.display = 'inline-flex';
+});
+el.addEventListener('mouseout', function handleMouseOut() {
+    hiddenEl.style.display = 'none';
+});
+
+
+const el_2 = document.getElementById('box_2');
+
+const hiddenEl_2 = document.getElementById('hidden_2');
+
+el_2.addEventListener('mouseover', function handleMouseOver() {
+    hiddenEl_2.style.display = 'inline-flex';
+});
+el_2.addEventListener('mouseout', function handleMouseOut() {
+    hiddenEl_2.style.display = 'none';
+});
+
+
+const el_3 = document.getElementById('box_3');
+
+const hiddenEl_3 = document.getElementById('hidden_3');
+
+el_3.addEventListener('mouseover', function handleMouseOver() {
+    hiddenEl_3.style.display = 'inline-flex';
+});
+el_3.addEventListener('mouseout', function handleMouseOut() {
+    hiddenEl_3.style.display = 'none';
+});
+
+
+const el_4 = document.getElementById('box_4');
+
+const hiddenEl_4 = document.getElementById('hidden_4');
+
+el_4.addEventListener('mouseover', function handleMouseOver() {
+    hiddenEl_4.style.display = 'inline-flex';
+});
+el_4.addEventListener('mouseout', function handleMouseOut() {
+    hiddenEl_4.style.display = 'none';
+});
